4979ebdcdd0e1ad8169f81666f95e81ac660456a
label <zhaolei86ok@sina.com>
2018-02-08 23:12:56 +0800

    修改推动广告逻辑
    Change-Id: I605a756db9deb051b6dacf7dec85c519f6129eb1
    Signed-off-by: label <zhaolei86ok@sina.com>

--- src/main/java/web/com/sva/web/controllers/PushAdController.java
+++ src/main/java/web/com/sva/web/controllers/PushAdController.java
@@ -9,9 +9,16 @@
 package com.sva.web.controllers;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.io.UnsupportedEncodingException;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
 import javax.servlet.http.HttpServletRequest;
 import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,6 +27,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import com.sva.common.ConvertUtil;
@@ -41,17 +49,17 @@ public class PushAdController
     private SystemService service;
     
     /** 
-     * @Title: uploadAndPush 
-     * @Description: 推送广告 
+     * @Title: upload 
+     * @Description: 上传广告 
      * @param image
      * @param txt
      * @param request
      * @return
      * @throws SQLException 
      */
-    @RequestMapping(value = "/uploadAndPush", method = {RequestMethod.POST})
+    @RequestMapping(value = "/upload", method = {RequestMethod.POST})
     public String uploadAndPush(
-            @RequestParam(value = "image", required = false) MultipartFile image,String txt,
+            @RequestParam(value = "image", required = false) MultipartFile image,
             HttpServletRequest request, RedirectAttributes model) throws SQLException{
         String result = "";
         String fileName = "";
@@ -78,15 +86,51 @@ public class PushAdController
             }
         }
         
-        if(StringUtils.isEmpty(txt)){
-            result = "文本不能为空！";
-        }else{
-            // 推送微信
-            service.pushAd(fileName, txt);
-            result = "消息已推送";
-        }
-        
         model.addFlashAttribute("info", result);
         return "redirect:/home/showPushAd";
+    }
+    
+    /**   
+     * @Title: getImageList   
+     * @Description: 获取广告图片列表   
+     * @param request
+     * @return：Map<String,Object>       
+     * @throws   
+     */ 
+    @RequestMapping(value = "/getImageList", method = {RequestMethod.POST})
+    @ResponseBody
+    public Map<String, Object> getImageList(HttpServletRequest request){
+        Map<String, Object> result = new HashMap<String, Object>();
+        // 广告文件夹
+        String path = request.getSession().getServletContext()
+                .getRealPath("/WEB-INF/upload/ad");
+        File file = new File(path);
+        String[] filelist = file.list();
+        
+        result.put("data", filelist);
+        return result;
+    }
+    
+    /**   
+     * @Title: pushWeixin   
+     * @Description: 微信推送广告接口   
+     * @param imgName
+     * @param txt
+     * @return：Map<String,Object>       
+     * @throws   
+     */ 
+    @RequestMapping(value = "/pushWeixin", method = {RequestMethod.POST})
+    @ResponseBody
+    public Map<String, Object> pushWeixin(String imgName, String txt){
+        Map<String, Object> result = new HashMap<String, Object>();
+        if(StringUtils.isEmpty(txt)){
+            result.put("data", "文本不能为空！");
+        }else{
+            // 推送微信
+            service.pushAd(imgName, txt);
+            result.put("data", "消息已推送");
+        }
+        
+        return result;
     }
 }
